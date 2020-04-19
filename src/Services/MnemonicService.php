c6e3ec2ebcca1aae5568da3b18538a547d49ef73
Raditz Farhan <raditzfarhan@gmail.com>
2020-04-19 23:33:37 +0800

    Add mnemonic userByWords method

--- src/Services/MnemonicService.php
+++ src/Services/MnemonicService.php
@@ -20,7 +20,7 @@ class MnemonicService
         $hashed_entropy = self::hash($mnemonic->entropy);
 
         // ensure the mnemonic is unique
-        while (UserSecurity::where('mnemonic_entropy', $hashed_entropy)->first()) {
+        while (UserSecurity::where('mnemonic_entropy', $hashed_entropy)->count()) {
             $mnemonic = BIP39::Generate($word_count);
             $hashed_entropy = self::hash($mnemonic->entropy);
         }
@@ -31,6 +31,8 @@ class MnemonicService
     /**
      * Use mnemonic codes to find entropy.
      *
+     * @param array $words
+     * 
      * @return FurqanSiddiqui\BIP39\Mnemonic
      */
     public function words(array $words)
@@ -41,6 +43,8 @@ class MnemonicService
     /**
      * Generate Mnemonic using specified Entropy.
      *
+     * @param string @entropy
+     * 
      * @return FurqanSiddiqui\BIP39\Mnemonic
      */
     public function entropy($entropy)
@@ -48,11 +52,39 @@ class MnemonicService
         return BIP39::Entropy($entropy);
     }
 
-    public function hash($value)
+    /**
+     * Generate a hash for mnemonic entropy.
+     *
+     * @param string $value
+     * 
+     * @return string
+     */
+    public function hash(string $value): string
     {
         $hash_key = config('rfauthenticator.key');
         $hash_algo = config('rfauthenticator.algo');
 
         return hash($hash_algo, $value . $hash_key);
+    }
+
+    /**
+     * Generate a hash for mnemonic entropy.
+     *
+     * @param array $words
+     * 
+     * @return string
+     */
+    public function userByWords(array $words)
+    {
+        try {
+            $mnemonic = self::words($words);
+            $hashed_entropy = self::hash($mnemonic->entropy);
+
+            if ($userSecurity = UserSecurity::where('mnemonic_entropy', $hashed_entropy)->first()) {
+                return  $userSecurity->model_type::find($userSecurity->model_id);
+            }
+        } catch (Exception $e) {
+            throw new UnexpectedValueException("Invalid words.");
+        }
     }
 }
